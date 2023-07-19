e6a4b5ea3e94461dd1bf89fc20aee061e21f2835
Roy Joseph <royjoseph.mec@gmail.com>
2023-07-19 18:38:38 +0530

    Solved the Nan case which was appearing, changed some styling, changed the code with top and bottom cards for returning the weather data.

---
+++ src/TopCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TopCard({weather}) {
+  if(weather === null) return " "
+  else
+  return (
+    <div className='weatherBox'>
+        <h1 className='temperature'>{Math.round(weather.tempC)}</h1>
+        <div className='generalInfo'>
+            <h1 className='condition'>{weather.condition}</h1>
+            <h1 className='location'>{weather.name}</h1>
+            <h1 className='country'>{weather.country}</h1>
+        </div>
+    </div>
+  )
+}
+
+export default TopCard
